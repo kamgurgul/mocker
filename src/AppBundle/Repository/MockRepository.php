22acbb6615539f6bd881dc1cc1320fd16d663318
kgurgul <kamgurgul@gmail.com>
2015-10-26 20:42:29 +0100

    Add support for multiple header

--- src/AppBundle/Repository/MockRepository.php
+++ src/AppBundle/Repository/MockRepository.php
@@ -17,20 +17,7 @@ class MockRepository extends EntityRepository
     public function getMockByUrl($mockUrl)
     {
         $result = $this->getEntityManager()->createQueryBuilder()
-            ->select('
-                m.id as mockId,
-                m.userId,
-                m.url,
-                m.method,
-                m.responseStatus,
-                m.body,
-                m.createdAt,
-                m.blocked,
-                m.deleted,
-                h.id as headerId,
-                h.headerKey,
-                h.headerValue
-            ')
+            ->select('m')
             ->from('AppBundle:Mock', 'm')
             ->leftJoin('m.headers', 'h')
             ->where('m.url = :url')
@@ -40,32 +27,10 @@ class MockRepository extends EntityRepository
             ->execute();
 
         if ($result != null) {
-            return $this->parseDBDataToObject($result);
+            return $result[0];
         } else {
             return null;
         }
-    }
-
-    private function parseDBDataToObject($dbMock)
-    {
-        $mock = new Mock();
-
-        $mock->setId($dbMock[0]['mockId']);
-        $mock->setUserId($dbMock[0]['userId']);
-        $mock->setUrl($dbMock[0]['url']);
-        $mock->setMethod($dbMock[0]['method']);
-        $mock->setResponseStatus($dbMock[0]['responseStatus']);
-        $mock->setBody($dbMock[0]['body']);
-        $mock->setCreatedAt($dbMock[0]['createdAt']);
-        $mock->setBlocked($dbMock[0]['blocked']);
-        $mock->setDeleted($dbMock[0]['deleted']);
-
-        $headers = new Header();
-        $headers->setHeaderKey($dbMock[0]['headerKey']);
-        $headers->setHeaderValue($dbMock[0]['headerValue']);
-        $mock->addHeader($headers);
-
-        return $mock;
     }
 
     public function saveMock(Mock $mock)
