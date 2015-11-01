3c5f6a3afa1cdd3e84bf2ed887463c1af3b8cd96
kgurgul <kamgurgul@gmail.com>
2015-11-01 22:43:48 +0100

    JS method for new headers

--- app/Resources/public/js/bootstrap/mocker.js
+++ app/Resources/public/js/bootstrap/mocker.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
+    jQuery('#add_header_button').click(function (e) {
+        e.preventDefault();
 
+        var headerList = jQuery('#headers_list');
+        var headerCount = headerList.size();
+
+        var newWidget = headerList.attr('data-prototype');
+        // replace the "__name__" used in the id and name of the prototype
+        // with a number that's unique to your emails
+        // end name attribute looks like name="contact[emails][2]"
+        newWidget = newWidget.replace(/__name__/g, headerCount);
+
+        // create a new list element and add it to the list
+        var newLi = jQuery('<li class="row"></li>').html(newWidget);
+        newLi.appendTo(headerList);
+    });
 });
 
 $("#submit_form_button").click(function () {
